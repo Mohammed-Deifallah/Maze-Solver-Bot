98d59bf57f067316ad52fac47fbb4bc0824a63e6
Michael <micoraafat96@gmail.com>
2019-01-26 19:36:02 +0200

    .

--- src/environment/Utils.java
+++ src/environment/Utils.java
@@ -1,18 +1,28 @@
 package environment;
 
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Utils {
 
-	public static Action selectAction(int i) {
-		switch(i) {
-		case 0:
-			return Action.NORTH;
-		case 1:
-			return Action.EAST;
-		case 2:
-			return Action.SOUTH;
-		case 3:
-			return Action.WEST;
+	public static boolean pathExists(Cell grid[][]) {
+		List<Point> stack = new ArrayList<Point>();
+		stack.add(grid[0][0].getPosition());
+		while(!stack.isEmpty()) {
+			Point point = stack.remove(stack.size() - 1);
+			if (grid[point.x][point.y].getType() == Type.END) {
+				return true;
+			}
+			// searching for valid neighbors cells
+			List<Action> actions = grid[point.x][point.y].getPossibleActions();
+			for (int i = 0; i < actions.size(); i++) {
+				Action action = actions.get(i);
+				stack.add(action.getTransition(grid[point.x][point.y]));
+			}
+			
 		}
-		return null; 
+		return false;
+		
 	}
 }
